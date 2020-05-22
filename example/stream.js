075da511767316eccbb57c05c1a697e90e5bb3db
Ryan Northey <ryan@synca.io>
2020-05-22 17:10:50 +0100

    example

---
+++ example/stream.js
@@ -0,0 +1,15 @@
+
+var my_header =  'Foo: foo';
+function header_inject(s) {
+    var req = '';
+    s.on('upload', function(data, flags) {
+        req += data;
+        var n = req.search('\n');
+        if (n != -1) {
+            var rest = req.substr(n + 1);
+            req = req.substr(0, n + 1);
+            s.send(req + my_header + '\r\n' + rest, flags);
+            s.off('upload');
+        }
+    });
+}
